6a0aa16552e6547a8a1fcf216a9d347e6a2679ae
Ritesh jha <rjamasterstudios@gmail.com>
2025-07-30 10:52:12 +0530

    engine is created

---
+++ Backend/models/story.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+class Story(Base):
+    pass
